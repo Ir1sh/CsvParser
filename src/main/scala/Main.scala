119cfff1f06592c7b1c56db31a3841addbfb9e39
Ir1sh <mmoore@iofficecorp.com>
2015-04-19 17:00:57 +0100

    added countries parser and saver

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,82 +1,19 @@
-import java.io.InputStream
 
-import slick.backend.DatabaseConfig
-import scala.concurrent.{Future, Await}
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-
-import org.parboiled2.ParserInput
-import parser.CsvParser
+import sources.countries.Countries
+import sources.countries.Countries.{CountryParser, Country, Countries}
 import slick.driver.PostgresDriver.api._
-import slick.lifted.{ProvenShape, ForeignKeyQuery}
 
-import scala.io.Source
-import scala.util.{Failure, Success, Try}
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 /**
  * Created by Mark on 3/29/15.
  */
 object Main {
-  private lazy val stream: InputStream = getClass.getResourceAsStream("CountryCode.csv")
-  private lazy val countryCodes = Source.fromInputStream(stream).mkString
-
-  //definition of countries table
-  case class Country(name: String, isoCode: String, id: Option[Int] = None)
-
-  class Countries(tag: Tag) extends Table[Country](tag, "Countries") {
-    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
-
-    // This is the primary key column
-    def name = column[String]("name")
-
-    def isoCode = column[String]("isoCode")
-
-    // Every table needs a * projection with the same type as the tabe's type parameter
-    def * = (name, isoCode, id.?) <>(Country.tupled, Country.unapply)
-  }
-
-  val countries = TableQuery[Countries]
-
   def main(args: Array[String]) {
     val db = Database.forConfig("pgLocalDev")
-    try {
-      val countries = TableQuery[Countries]
-      val irl = Country("Ireland", "ei")
-      val eng = Country("England", "en")
-      val sc = Country("Scotland", "sc")
-      val wl = Country("Wales", "wl")
-
-        // Insert some coffees (using JDBC's batch insert feature)
-        val insertAction: DBIO[Option[Int]] = countries ++= Seq(irl, eng)
-
-        val insertAndPrintAction: DBIO[Unit] = insertAction.map { countriesInsertResult =>
-          // Print the number of rows inserted
-          countriesInsertResult foreach { numRows =>
-            println(s"Inserted $numRows rows into the Countries table")
-          }
-        }
-        /*val allCountriesAction: DBIO[Seq[(Int, String, String)]] =
-          TableQuery[Countries].result*/
-
-        val combinedAction: DBIO[Unit] =
-          insertAndPrintAction
-
-        val combinedFuture: Future[Unit] =
-          db.run(combinedAction)
-
-        combinedFuture.map { allSuppliers =>
-          println(allSuppliers)
-        }
-
-
-      Await.result(combinedFuture, Duration.Inf)
-    } finally db.close()
-    val p = new CsvParser(ParserInput(countryCodes), ",").file.run()
-    val countries: List[(String, String)] = p match {
-      case Success(x) => x.asInstanceOf[Vector[Vector[String]]].toList.map { case Vector(iso, nm) => Tuple2(iso, nm) }
-      case Failure(e) => List[(String, String)]()
-    }
-    countries.map { x => Console.println(x._1 + ", " + x._2); x }
+    val clist = CountryParser.run
+    Countries.save(clist, db)
   }
 
 }
--- src/main/scala/sources/countries/Countries.scala
+++ src/main/scala/sources/countries/Countries.scala
@@ -1,8 +1,78 @@
 package sources.countries
 
+
+import java.io.InputStream
+import org.parboiled2.ParserInput
+import parser.CsvParser
+import slick.driver.PostgresDriver.api._
+import slick.jdbc.meta.MTable
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Future, Await}
+import scala.io.Source
+import scala.util.{Failure, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
+
 /**
  * Created by Mark on 4/19/15.
  */
-class Countries {
+object Countries {
 
+  //definition of countries table
+  case class Country(name: String, isoCode: String, id: Option[Int] = None)
+
+  class Countries(tag: Tag) extends Table[Country](tag, "Countries") {
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+
+    // This is the primary key column
+    def name = column[String]("name")
+
+    def isoCode = column[String]("isoCode")
+
+    // Every table needs a * projection with the same type as the table's type parameter
+    def * = (name, isoCode, id.?) <> (Country.tupled, Country.unapply)
+  }
+
+  val countries = TableQuery[Countries]
+
+  def save(ct: List[(String, String)], db: Database): Unit = {
+
+    try {
+      val createIfNotExists = {
+        println("gonna try and print")
+        DBIO.seq(
+          MTable.getTables(countries.baseTableRow.tableName).map(result => if(result.isEmpty) db.run(countries.schema.create))
+        )
+      }
+
+      val setUpFuture: Future[Unit] = db.run(createIfNotExists)
+      val f = setUpFuture.flatMap { _ =>
+        val insertAction: DBIO[Option[Int]] = countries ++= ct.map { c => Country(c._2, c._1)}
+        val insertAndPrintAction: DBIO[Unit] = insertAction.map { countriesInsertResult =>
+          // Print the number of rows inserted
+          countriesInsertResult foreach { numRows =>
+            println(s"Inserted $numRows rows into the Countries table")
+          }
+        }
+        db.run(insertAndPrintAction)
+      }
+      Await.result(f, Duration.Inf)
+    } finally db.close()
+  }
+
+  object CountryParser {
+    val stream: InputStream = getClass.getResourceAsStream("/CountryCode.csv")
+    println("got stream")
+    val countryCodes = Source.fromInputStream(stream).mkString
+    println("got string")
+    def run: List[(String, String)] = {
+      val p = new CsvParser(ParserInput(countryCodes), ",").file.run()
+      val countries: List[(String, String)] = p match {
+          //we need to drop the first row which is just column names from the csv that is why we end with .tail
+        case Success(x) => x.asInstanceOf[Vector[Vector[String]]].toList.map { case Vector(iso, nm) => Tuple2(iso, nm) }.tail
+        case Failure(e) => List[(String, String)]()
+      }
+      countries
+    }
+  }
 }
+
